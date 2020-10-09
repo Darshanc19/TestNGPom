ac7b836866fcdad6f7419db61ee67fbc751b849a
Darshan C <darshanc19@gmail.com>
2020-10-09 12:31:28 +0530

    its real

---
+++ src/testNG/First.java
@@ -0,0 +1,20 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class First {
+	@Test
+	
+public void Demo() {
+		System.out.println("shashikala");
+		
+	}
+	@Test
+	public void Sample() {
+		System.out.println("Darshan");
+	}
+	@Test
+	public void Example() {
+		System.out.println("Varun");
+	}
+}
---
+++ src/testNG/Second.java
@@ -0,0 +1,13 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class Second {
+	
+	@Test
+	public void Contentment() {
+		System.out.println("It's my first project");
+		
+	}
+
+}
